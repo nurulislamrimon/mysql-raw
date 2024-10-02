f9b6ea0d3b8eb6e294e6be14f1b8bf2ab3731139
nurulislamrimon <nurulislamrimon@gmail.com>
2024-10-02 15:57:40 +0600

    server graceful exit and database connection implimented

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,63 @@
 import express from "express";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+// create app
 const app = express();
 const port = process.env.port ?? 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// configuration
+dotenv.config();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const main = async () => {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: "softmind_ni_test",
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error("Error connecting to database: ", err);
+      return;
+    }
+    console.log("Connected to database!");
+  });
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+};
+
+main();
+
+// handle unhandled errors
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// handle uncought Promise
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+  process.exit(1);
+});
+
+// handle sigterm and sigint
+process.on("SIGTERM", () => {
+  console.log("SIGTERM received, shutting down...");
+  process.exit(0);
+});
+
+process.on("SIGINT", () => {
+  console.log("SIGINT received, shutting down...");
+  process.exit(0);
 });
